c32f85a36cf0151ed4ff71a7425b6f6050cf3a8e
rsundar <rohan.s.1987@gmail.com>
2019-11-04 17:32:09 +0530

    Updated the main.rb file to conform to the comments made by the reviewr.

--- bin/main.rb
+++ bin/main.rb
@@ -6,6 +6,18 @@ game_mode = gets.chomp
 
 puts "You selected #{game_mode}"
 
+if game_mode == 1
+    puts "Sorry AGI has not been invented yet, check back in 2020!"
+elsif game_mode == 2
+    puts "Enter the name of Player 1:"
+    player1 = gets.chomp
+
+    puts "Enter the name of Player 2:"
+    player2 = gets.chomp
+else
+    puts "Please select (1) or (2)"
+end
+
 puts "Select (X) or (O)"
 x_or_o = gets.chomp
 
@@ -14,14 +26,19 @@ if (x_or_o != 'x' || x_or_o !='X' || x_or_o != 'o' || x_or_o != 'O')
     x_or_o = gets.chomp
 end
 
-# Main Game Loop, for now we countdown from 10 to 0 to test user interactivity
 # We will ask the user to enter a grid co ordinate between 1 and 9 and display the 
 # board/game state and request player 2 to enter the same and continue until the end of
 # the game (which happens when a player wins or all the cells are filled and no one has won).
 
-i = 10
-
-i.times do
+game_on =true
+num_of_turns = 9
+#Primary game loop
+while game_on
+    #loop through the turns until the game is won or 9 turns have been exhausted.
     puts "Enter Co-ordinate: "
     input = gets.chomp
+
+    if game_won || num_of_turns == 0
+        game_on = false
+    end 
 end
