7566a6fd410dc41a4ce367e681c35e0f98fdfe09
rsundar <rohan.s.1987@gmail.com>
2019-11-07 21:05:37 +0530

    Update

--- bin/main.rb
+++ bin/main.rb
@@ -1,35 +1,53 @@
-puts "Welcome to Tic Tac Toe!"
-puts "-----------------------"
+#! /usr/bin/env ruby
 
-puts "Play a game with (1) Computer, (2) Your Friend:"
-game_mode = gets.chomp
+#frozen_string_literal: true
 
-puts "You selected #{game_mode}"
+def welcome
+    puts " Welcome to the tic-tac-toe game!.
+    1. The game is played on a grid that's 3 squares by 3 squares.
+    2. You are X or O, your friend is O or X.
+    3. Players take turns putting their marks in empty squares.
+    4. The first player to get 3 in a row wins.
+    5. When all 9 squares are full, the game is over. If no player has 3."
+end
+welcome
 
-if game_mode == 1
-    puts "Sorry AGI has not been invented yet, check back in 2020!"
-elsif game_mode == 2
-    puts "Enter the name of Player 1:"
-    player1 = gets.chomp
-
-    puts "Enter the name of Player 2:"
-    player2 = gets.chomp
-else
-    puts "Please select (1) or (2)"
+def show_board(cell)
+    puts ''
+    puts '---+---+---'
+    cell.each do |i|
+        line = ''
+        i.each_with_index do |j,k|
+            line += "#{j} #{'|'}" if k!=2
+            line += "#{j}" if k==2
+        end
+        puts line
+        puts '---+---+---'
+    end
+    puts ''
 end
 
-puts "Select (X) or (O)"
-x_or_o = gets.chomp
+def name_valid?(name)
+    loop do
+        valid = name.empty?
+        return name unless valid
+    end
+end
 
-if (x_or_o != 'x' || x_or_o !='X' || x_or_o != 'o' || x_or_o != 'O')
-    puts "Please select (X) or (O)"
-    x_or_o = gets.chomp
+def valid_element?(element)
+    loop do
+        valid = %w[X O]
+        return element if element.include? valid
+        puts "Please Enter X or O in upper case or lower case."
+        element = gets.chomp.upcase!
+    end
 end
 
 # We will ask the user to enter a grid co ordinate between 1 and 9 and display the 
 # board/game state and request player 2 to enter the same and continue until the end of
 # the game (which happens when a player wins or all the cells are filled and no one has won).
 
+board = 
 game_on =true
 num_of_turns = 9
 #Primary game loop
