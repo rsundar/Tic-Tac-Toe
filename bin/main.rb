283d764918ac93ea1db2fb98e33d70760e6123b4
rsundar <rohan.s.1987@gmail.com>
2019-11-11 22:59:18 +0530

    Updated display module and user interface.

--- bin/main.rb
+++ bin/main.rb
@@ -1,27 +1,5 @@
-puts "Welcome to Tic Tac Toe!"
-puts "-----------------------"
+#! /usr/bin/env ruby
+# frozen_string_literal: true
 
-puts "Play a game with (1) Computer, (2) Your Friend:"
-game_mode = gets.chomp.upcase!
+game_count = 0
 
-puts "You selected #{game_mode}"
-
-puts "Select (X) or (O)"
-x_or_o = gets.chomp
-
-if (x_or_o != 'x' || x_or_o !='X' || x_or_o != 'o' || x_or_o != 'O')
-    puts "Please select (X) or (O)"
-    x_or_o = gets.chomp
-end
-
-# Main Game Loop, for now we countdown from 10 to 0 to test user interactivity
-# We will ask the user to enter a grid co ordinate between 1 and 9 and display the 
-# board/game state and request player 2 to enter the same and continue until the end of
-# the game (which happens when a player wins or all the cells are filled and no one has won).
-
-i = 10
-
-i.times do
-    puts "Enter Co-ordinate: "
-    input = gets.chomp
-end
